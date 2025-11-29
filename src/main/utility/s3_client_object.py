f2af917877edea013afedb9ec1c233abe9a628f2
Praveen Kumar <139849701+Praveencse79@users.noreply.github.com>
2025-11-29 20:30:24 +0530

    Update s3_client_object.py
    Updating the s3 files.

--- src/main/utility/s3_client_object.py
+++ src/main/utility/s3_client_object.py
@@ -1,13 +1,9 @@
 import boto3
-import os
 
 class S3ClientProvider:
-    def _init_(self):
-        # Read keys from OS environment variables
-        self.aws_access_key = os.getenv("AWS_ACCESS_KEY_ID")
-        self.aws_secret_key = os.getenv("AWS_SECRET_ACCESS_KEY")
-
-        # Create session without hardcoding keys
+    def __init__(self, aws_access_key=None, aws_secret_key=None):
+        self.aws_access_key = aws_access_key
+        self.aws_secret_key = aws_secret_key
         self.session = boto3.Session(
             aws_access_key_id=self.aws_access_key,
             aws_secret_access_key=self.aws_secret_key
